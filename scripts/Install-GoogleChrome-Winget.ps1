49f2e32a28a4b632582b0bb97239afe1bb49d1a8
Dane Gauthier <danegauthier@gmail.com>
2025-05-10 04:02:19 -0400

    Commit

--- scripts/Install-GoogleChrome-Winget.ps1
+++ scripts/Install-GoogleChrome-Winget.ps1
@@ -1,1 +1,10 @@
+# Check if winget is installed
+if (-not (Get-Command winget -ErrorAction SilentlyContinue)) {
+    Write-Host "winget not found. Installing winget..."
+    Invoke-WebRequest https://raw.githubusercontent.com/asheroto/winget-installer/master/winget-install.ps1 -UseBasicParsing | Invoke-Expression
+} else {
+    Write-Host "winget is already installed."
+}
+
+# Install Google Chrome silently
 winget install --id=Google.Chrome --silent --accept-package-agreements --accept-source-agreements
