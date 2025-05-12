8856da90c53aa07f55a8570ae442c5d76771e3b9
Dane Gauthier <danegauthier@gmail.com>
2025-05-12 19:48:26 -0400

    Commit

--- startstop-app/start-app.ps1
+++ startstop-app/start-app.ps1
@@ -1,15 +1,13 @@
 param (
     [string]$ResourceGroupName = "Dev-RG-Nerdio",
     [string]$AppService = "nmw-app-72db5tuhrtrs4"
-
 )
 
-$app1 = Get-AzWebApp -ResourceGroupName $ResourceGroupName -Name $AppService1
+$app = Get-AzWebApp -ResourceGroupName $ResourceGroupName -Name $AppService
 
-if ($app1.State -eq "Stopped") {
+if ($app.State -eq "Stopped") {
     Write-Output "$AppService is not running. Starting"
     Start-AzWebApp -ResourceGroupName $ResourceGroupName -Name $AppService > $null
 } else {
     Write-Output "$AppService is already started."
 }
-
--- startstop-app/stop-app.ps1
+++ startstop-app/stop-app.ps1
@@ -1,15 +1,13 @@
 param (
     [string]$ResourceGroupName = "Dev-RG-Nerdio",
     [string]$AppService = "nmw-app-72db5tuhrtrs4"
-
 )
 
-$app1 = Get-AzWebApp -ResourceGroupName $ResourceGroupName -Name $AppService
+$app = Get-AzWebApp -ResourceGroupName $ResourceGroupName -Name $AppService
 
-if ($app1.State -eq "Running") {
+if ($app.State -eq "Running") {
     Write-Output "$AppService is running. Stopping..."
     Stop-AzWebApp -ResourceGroupName $ResourceGroupName -Name $AppService > $null
 } else {
     Write-Output "$AppService is already stopped."
 }
-
